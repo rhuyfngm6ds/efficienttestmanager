e5e2f62b9826b28e2492f317331fea5c647de6ef
rhuyfngm6ds <csjcolinleevy65@hotmail.com>
2024-05-08 15:13:56 +0700

    start project

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const radixSort = (arr) => {
-  const getDigit = (num, place) =>
-    Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
-  const digitCount = (num) =>
-    num === 0 ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
-  const mostDigits = (arr) => Math.max(...arr.map((num) => digitCount(num)));
-  const maxDigits = mostDigits(arr);
-  for (let k = 0; k < maxDigits; k++) {
-    let digitBuckets = Array.from({ length: 10 }, () => []);
-    for (let i = 0; i < arr.length; i++) {
-      digitBuckets[getDigit(arr[i], k)].push(arr[i]);
-    }
-    arr = [].concat(...digitBuckets);
+const quickSortRandomPivot = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
   }
-  return arr;
+  const pivotIndex = Math.floor(Math.random() * arr.length);
+  const pivot = arr[pivotIndex];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== pivotIndex) {
+      if (arr[i] <= pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+  }
+  return quickSortRandomPivot(left).concat(pivot, quickSortRandomPivot(right));
 };
