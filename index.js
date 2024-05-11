21d24835f8e7447871a380f48de0a80a317e569b
rhuyfngm6ds <csjcolinleevy65@hotmail.com>
2024-05-12 04:06:05 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const currentDate = new Date();
-console.log(currentDate); // Fri 2024-04-05T09:17:00.000Z
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
+};
